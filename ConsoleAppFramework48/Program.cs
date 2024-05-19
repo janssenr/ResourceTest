5e0e2148469c413e1b2119fd34f8fe1bae8215a2
Rob Janssen <rob.janssen1977@gmail.com>
2024-05-19 13:01:19 +0200

    Add files via upload

---
+++ ConsoleAppFramework48/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleAppFramework48
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string value = Resources.ResourceFile.TextFile;
+
+            Console.WriteLine(value);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleAppNet8/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ConsoleAppNet8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string value = Resources.ResourceFile.TextFile;
+
+            Console.WriteLine(value);
+        }
+    }
+}
